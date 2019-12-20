8a28a10b903842d6a7058ce79182eeac8ef5c223
klee8880 <klee8880@gmail.com>
2019-12-19 21:35:44 -0600

    Create InternalPath.cs

---
+++ Navigation/InternalPath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cleaning_Bot_Conversion.Navigation
+{
+    class InternalPath
+    {
+        public Stack<Coordinates> Path;
+        private float totalCost { get; set; }
+
+        public InternalPath()
+        {
+            Path = new Stack<Coordinates>();
+        }
+
+        //Stack Manipulation
+        public void Push(Coordinates next) => Path.Push(next);
+        public Coordinates Pop() => Path.Pop();
+        public Coordinates Peek() => Path.Peek();
+        public bool IsEmpty() => Path.Count <= 0;
+    }
+}
