b383d270655e97a7c2a531c062bb6d7c5d118b14
klee8880 <klee8880@gmail.com>
2019-12-21 17:50:19 -0600

    Setup Sensor Package

---
+++ Sensors/ISensorPackage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Cleaning_Bot_Conversion.Navigation;
+
+namespace Cleaning_Bot_Conversion.Sensors
+{
+    public interface ISensorPackage
+    {
+        bool CollisionNorth(Coordinates current);
+        bool CollisionEast(Coordinates current);
+        bool CollisionSouth(Coordinates current);
+        bool CollisionWest(Coordinates current);
+        bool DirtDetected(Coordinates current);
+        Coordinates[] StationsDetected();
+        void CleanTile(Coordinates target);
+
+    }
+}
