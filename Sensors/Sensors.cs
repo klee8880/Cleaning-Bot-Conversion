6946ac286c5e6a52be01618dfcdc61094127b998
klee8880 <klee8880@gmail.com>
2019-12-23 20:21:38 -0600

    Create Sensors.cs

---
+++ Sensors/Sensors.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cleaning_Bot_Conversion.Navigation;
+
+namespace Cleaning_Bot_Conversion.Sensors
+{
+    class Sensors : ISensorPackage
+    {
+        public void CleanTile(Coordinates target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CollisionEast(Coordinates current)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CollisionNorth(Coordinates current)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CollisionSouth(Coordinates current)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CollisionWest(Coordinates current)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DirtDetected(Coordinates current)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Coordinates[] StationsDetected()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
