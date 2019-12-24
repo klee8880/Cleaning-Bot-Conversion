857888fd0f094ff28e4b8f69751b302109751ce8
klee8880 <klee8880@gmail.com>
2019-12-23 20:24:09 -0600

    Create TileType.cs

---
+++ Navigation/TileType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cleaning_Bot_Conversion.Navigation
+{
+    public enum TileType
+    {
+        Obstacle = -1,
+        High = 3,
+        Low = 2,
+        Bare = 1,
+        Unknown = 3
+    }
+}
